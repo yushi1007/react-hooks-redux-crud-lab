d3cc54f517a0f29e537c3b58d07e8457146b7b91
Yu Shi <yushiys95@gmail.com>
2021-04-27 22:26:30 -0400

    commit tree 30be0bc15cf08389845472da09c791f5688ea383

--- src/features/reviews/Review.js
+++ src/features/reviews/Review.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { reviewRemoved } from "./reviewsSlice";
 
 function Review({ review }) {
+  const dispatch = useDispatch();
+
+  function handleDeleteClick() {
+    dispatch(reviewRemoved(review.id));
+  }
+
   return (
     <div>
       <li>{review.comment}</li>
-      <button> Delete Review </button>
+      <button onClick={handleDeleteClick}> Delete Review </button>
     </div>
   );
 }
