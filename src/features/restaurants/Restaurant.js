851747e695d1ccfff19a9fe8f4b79dde3b977ad4
Yu Shi <yushiys95@gmail.com>
2021-04-27 22:26:36 -0400

    Done.

--- src/features/restaurants/Restaurant.js
+++ src/features/restaurants/Restaurant.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import ReviewsContainer from "../reviews/ReviewsContainer";
+import { restaurantRemoved } from "./restaurantsSlice";
 
 function Restaurant({ restaurant }) {
+  const dispatch = useDispatch();
+  function handleDeleteClick() {
+    dispatch(restaurantRemoved(restaurant.id));
+  }
   return (
     <div>
       <li>
         {restaurant.name}
-        <button> Delete Restaurant </button>
-        <ReviewsContainer restaurant={restaurant} />
+        <button onClick={handleDeleteClick}>Delete Restaurant</button>
+        <ReviewsContainer restaurantId={restaurant.id} />
       </li>
     </div>
   );
